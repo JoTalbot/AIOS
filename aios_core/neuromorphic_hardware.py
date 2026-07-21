8f79deef309942b627f38260402f8f8872cecba1
JoTalbot <jo.talbot@gmail.com>
2026-07-21 10:38:11 +0000

    feat: Spiking NN, Neuromorphic Hardware, BCI

---
+++ aios_core/neuromorphic_hardware.py
@@ -0,0 +1,23 @@
+"""Neuromorphic Hardware Abstraction for AIOS"""
+
+from typing import Dict, List
+
+
+class NeuromorphicChip:
+    """Abstract neuromorphic hardware interface."""
+
+    def __init__(self, cores: int = 128, neurons_per_core: int = 256):
+        self.cores = cores
+        self.neurons_per_core = neurons_per_core
+        self.energy_per_spike = 0.000001  # pJ
+
+    def map_network(self, num_neurons: int) -> Dict:
+        cores_needed = (num_neurons + self.neurons_per_core - 1) // self.neurons_per_core
+        return {
+            "cores_used": min(cores_needed, self.cores),
+            "neurons_mapped": num_neurons,
+            "power_estimate_w": cores_needed * 0.1
+        }
+
+    def stats(self) -> dict:
+        return {"cores": self.cores, "neurons_per_core": self.neurons_per_core}
