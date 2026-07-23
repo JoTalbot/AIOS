b441539f9bd59bc75523adfa9921c98e93df88fa
JoTalbot <jo.talbot@gmail.com>
2026-07-23 18:04:55 +0000

    docs: add doctor() docstrings to 4 platform bootstrap modules

--- aios_core/modules/viber/bootstrap.py
+++ aios_core/modules/viber/bootstrap.py
@@ -20,6 +20,7 @@ class ViberBootstrap:
         self.which = which
 
     def doctor(self) -> None:
+        """Run readiness checks for this platform agent."""
         return platform_doctor(
             PLATFORM,
             PACKAGE,
