8daf0dcfe1714068a3db6f46563b5bd248e33aa2
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-02 03:19:48 +0300

    Add AIOS v5 Android emulator skill foundation

---
+++ aios_core/v5/skills/android/emulator.py
@@ -0,0 +1,14 @@
+class EmulatorController:
+    """Android emulator control adapter foundation."""
+
+    def __init__(self):
+        self.running = False
+
+    def start(self):
+        self.running = True
+
+    def stop(self):
+        self.running = False
+
+    def status(self):
+        return {"running": self.running}
