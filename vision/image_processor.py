8b82606800941efa57efca21b84509bc23c4ffe9
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-02 05:10:10 +0300

    Add AIOS image processor foundation

---
+++ vision/image_processor.py
@@ -0,0 +1,8 @@
+class ImageProcessor:
+    """AIOS image processing foundation."""
+
+    def process(self, image):
+        return {
+            "image": image,
+            "processed": True
+        }
