6e14e99110c9ce34e161dcf94fc90127cdaa1c11
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-02 03:54:41 +0300

    Add AIOS v5 improvement cycle foundation

---
+++ intelligence/learning/improvement_cycle.py
@@ -0,0 +1,8 @@
+class ImprovementCycle:
+    """Continuous improvement cycle foundation."""
+
+    def run(self, state):
+        return {
+            "state": state,
+            "cycle": "completed"
+        }
