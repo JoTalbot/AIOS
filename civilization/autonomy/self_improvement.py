99a1422e4a37abd6e32573b9c4b857f5e326c525
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-02 04:14:57 +0300

    Add AIOS v6 self improvement foundation

---
+++ civilization/autonomy/self_improvement.py
@@ -0,0 +1,9 @@
+class SelfImprovement:
+    """Self improvement foundation."""
+
+    def improve(self, capability, feedback):
+        return {
+            "capability": capability,
+            "feedback": feedback,
+            "improved": True
+        }
