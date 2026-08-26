31c67846a6777fade2627b4e6e376c1391a69f20
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-26 18:18:32 +0300

    feat(kernel): add component registry

---
+++ core/kernel/registry.py
@@ -0,0 +1,12 @@
+class KernelRegistry:
+    def __init__(self):
+        self.components = {}
+
+    def register(self, name, component):
+        self.components[name] = component
+
+    def get(self, name):
+        return self.components.get(name)
+
+    def list_components(self):
+        return list(self.components.keys())
