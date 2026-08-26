76b655faafba439687b704555668e189b751ef59
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-26 15:53:55 +0300

    Add plugin registry foundation

---
+++ core/plugins/registry.py
@@ -0,0 +1,12 @@
+"""Plugin registry foundation."""
+
+
+class PluginRegistry:
+    def __init__(self):
+        self.plugins = {}
+
+    def register(self, name, plugin):
+        self.plugins[name] = plugin
+
+    def get(self, name):
+        return self.plugins.get(name)
