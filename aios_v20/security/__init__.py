952df65110444689e4cbc1b7af1db292cc0cec0d
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-26 13:20:51 +0300

    Add AIOS v20.15 security package

---
+++ aios_v20/security/__init__.py
@@ -0,0 +1,9 @@
+from .threat_detection import ThreatDetector, ThreatAssessment
+from .trust import TrustManager, TrustScore
+
+__all__ = [
+    "ThreatDetector",
+    "ThreatAssessment",
+    "TrustManager",
+    "TrustScore",
+]
