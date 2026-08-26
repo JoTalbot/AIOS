f0232e26425917eddaee5a8706ee85454504f87e
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-26 15:42:26 +0300

    Add runtime dependency container

---
+++ core/runtime/container.py
@@ -0,0 +1,11 @@
+"""Simple dependency container for AIOS components."""
+
+class ServiceContainer:
+    def __init__(self):
+        self.services = {}
+
+    def register(self, name, service):
+        self.services[name] = service
+
+    def resolve(self, name):
+        return self.services.get(name)
